26530bba789a2e9ec6750d3b7edd572ba9179dfa
usuario <usuario@LAPTOP-OIRSR9JK>
2020-05-13 12:52:14 -0600

    Primer proyecto en Github que muestra saludos primer cambio

--- src/EjemploGitHub.java
+++ src/EjemploGitHub.java
@@ -3,7 +3,9 @@ public class EjemploGitHub {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Bienvenido");
-        
+        //primera actualizacion desde netbeans
+        System.out.println("Hi");
+        System.out.println("*************Holaa");
     }
     
 }
