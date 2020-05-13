bc260f2dd55f3fcd1de8ada8cf8a927bb04ae191
usuario <usuario@LAPTOP-OIRSR9JK>
2020-05-13 12:30:01 -0600

    Primer proyecto en Github que muestra saludos

---
+++ src/EjemploGitHub.java
@@ -0,0 +1,9 @@
+//Ejemplo para subir a Github//
+public class EjemploGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("Bienvenido");
+        
+    }
+    
+}
